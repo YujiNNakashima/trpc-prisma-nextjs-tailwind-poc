881edb392640fa1a656843f7049c09db7ab80a3a
Yuji <yujiricardo@gmail.com>
2022-07-16 23:51:04 -0300

    feat: add reply

--- src/components/comment-section/CommentSection.tsx
+++ src/components/comment-section/CommentSection.tsx
@@ -21,10 +21,28 @@ type Reply = Comment & {
 
 export const CommentSection: React.FC<{data: Comment, reply?: boolean}> = ({data, reply}) => {
 
-  if(reply) return <h1>dskljaidjaisdhafdhiofh</h1>
+  if(reply) return (
+    <div className="mt-4 ml-16 
+    px-4 py-4 bg-neutral-very-light-gray max-w-3xl rounded-md 
+    "
+    >
+    <div className="flex flex-row">
+     <p className="pr-4">upvote</p>
+     <div className="flex flex-col">
+       <div className="flex flex-row">
+         <div className="imagem"></div>
+         <p>{data.user.username}</p>
+         <p>{data.createdAt}</p>
+         <p>reply</p>
+       </div>
+       <p>{data.content}</p>
+     </div>
+    </div>
+   </div>
+  )
 
   return (
-    <div className="container mx-auto px-4 py-4 bg-neutral-very-light-gray max-w-3xl rounded-md">
+    <div className="mx-auto mt-4 px-4 py-4 bg-neutral-very-light-gray max-w-3xl rounded-md">
      <div className="flex flex-row">
       <p className="pr-4">upvote</p>
       <div className="flex flex-col">
@@ -34,7 +52,7 @@ export const CommentSection: React.FC<{data: Comment, reply?: boolean}> = ({data
           <p>{data.createdAt}</p>
           <p>reply</p>
         </div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum inventore nobis rerum numquam dolorem repellendus tenetur maxime. Illo culpa quia maiores amet. Pariatur quia natus nostrum molestias itaque nam, iusto odio cumque error corrupti accusamus tenetur ipsum aut, esse ut alias voluptatem neque inventore placeat sunt ullam beatae, recusandae deserunt! Magni, veritatis neque consequatur fugiat amet molestias quis asperiores reprehenderit nisi, optio corporis consectetur laboriosam provident blanditiis doloribus totam! Molestiae nulla fugiat a quas, beatae placeat itaque, possimus, consequuntur facere corporis iste omnis incidunt alias minus odit aliquam animi atque.</p>
+        <p>{data.content}</p>
       </div>
      </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,24 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex items-center justify-center">
-        <div className="mb-10">
+        <div className="mb-10 max-w-2xl">
         {
           commentsData.comments.map((comment, i) => {
             return (
               <>
               <CommentSection key={`key-${i}`} data={comment}/>
               {comment?.replies.map((reply, i )=> (
+                <>
+                <div className="relative  
+                before:absolute
+                before:bg-primary-light-grayish-blue
+                before:min-h-full		
+                before:w-1
+                before:left-8"
+                >
                 <CommentSection key={`key-reply-${i}`} data={reply} reply/>
+                </div>
+                </>
               ))}
               </>
             )
